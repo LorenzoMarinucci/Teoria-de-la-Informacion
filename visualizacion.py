1712bfdb04f35c2b1817ae66551e9849f631a1e4
Lucia <62451140+LuPalomeque@users.noreply.github.com>
2020-09-26 20:10:22 -0300

    Modificacion visualizacion

--- visualizacion.py
+++ visualizacion.py
@@ -1,16 +1,20 @@
 def mostrar_distribucion(fuenteOriginal,fuenteSimulada):
-    print("Distribución de probabilidades en cada fuente\n")
+    print("_____________________________________________")
+    print("\nDistribución de probabilidades en cada fuente\n")
     print("Símbolo   Fuente original   Fuente simulada")
     for simbolo in fuenteOriginal:
         print("{:^7}   {:^15.4f}   {:^16.4f}".format(simbolo, fuenteOriginal[simbolo], fuenteSimulada[simbolo]))
 
 def mostrar_informacion(fuenteOriginal,informacionFuenteOriginal,informacionFuenteSimulada):
+    print("_____________________________________________")
     print("\nInformación otorgada por cada símbolo\n")
     print("Símbolo   Fuente original   Fuente simulada")
     for simbolo in fuenteOriginal:
         print("{:^7}   {:^15.4f}   {:^16.4f}".format(simbolo, informacionFuenteOriginal[simbolo], informacionFuenteSimulada[simbolo]))
 
 def mostrar_entropia(entropiaFuenteOriginal,entropiaFuenteSimulada):
+    print("_____________________________________________")
     print("\nEntropia de cada fuente\n")
     print("Fuente original   Fuente simulada\n")
     print("{:^15.4f}   {:^16.4f}".format(entropiaFuenteOriginal, entropiaFuenteSimulada))
+    print("_____________________________________________")
