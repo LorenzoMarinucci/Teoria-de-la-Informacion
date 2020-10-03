e782db6db25440efe603735465f0b3b5f87837ff
lorenzo <lorenmarinucci@gmail.com>
2020-10-02 21:36:48 -0300

    Virtual Environment con numpy

--- Markov/modulos/calculos.py
+++ Markov/modulos/calculos.py
@@ -1,16 +1,16 @@
 import math
 def calculoEntropiaMarkov(vectorV, matriz):
-    tamanio = len(vectorV);
-    total = 0;
+    tamanio = len(vectorV)
+    total = 0
     for j in range(tamanio):
-        sumFilas = 0;
+        sumFilas = 0
         for i in range(tamanio):
-            probAct = matriz[i][j];
+            probAct = matriz[i][j]
             if(probAct != 0):
-                sumFilas += probAct * math.log(1/probAct,2);
-        total += sumFilas * vectorV[j];
-    return total;
+                sumFilas += probAct * math.log(1/probAct,2)
+        total += sumFilas * vectorV[j]
+    return total
 
-vector = [1/3,0.5,1/6];
-matriz = [[0.5,1/3,0],[0.5,1/3,1],[0,1/3,0]];
-print(calculoEntropiaMarkov(vector, matriz));
+# vector = [1/3,0.5,1/6]
+# matriz = [[0.5,1/3,0],[0.5,1/3,1],[0,1/3,0]]
+# print(calculoEntropiaMarkov(vector, matriz))
