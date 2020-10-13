1571a933c2eb01a63f4fc6d983f117acdb62d410
Lucia <62451140+LuPalomeque@users.noreply.github.com>
2020-10-13 20:41:40 -0300

    Calculos

--- Codificación/main.py
+++ Codificación/main.py
@@ -1,7 +1,7 @@
 import sys
-from lectura import leerFuente
-from codificacion import generarCodificacion
-from calculos import *
+from Codificación.modulos.lectura  import leerFuente
+from Codificación.modulos.codificacion import generarCodificacion
+from Codificación.modulos.calculos import entropia, longitudMedia, isCompacto, cumpleKraft
 
 def main():
     if len(sys.argv < 2):
@@ -11,9 +11,9 @@ def main():
     codificacion = generarCodificacion(fuente)
 
     print('Entropía de la fuente: ', entropia(fuente.values()))
-    print('Longitud media: ', longitudMedia())
-    print('El código es compacto: ', isCompacto())
-    print('Cumple la inecuación de Kraft: ', cumpleKraft())
+    print('Longitud media: ', longitudMedia(fuente.values(),codificacion))
+    print('El código es compacto: ', isCompacto(fuente.values(),codificacion))
+    print('Cumple la inecuación de Kraft: ', cumpleKraft(codificacion))
 
 if __name__ == '__main__':
     main()
--- Codificación/modulos/calculos.py
+++ Codificación/modulos/calculos.py
@@ -3,11 +3,11 @@ from math import log
 def entropia(probabilidadSimbolos):
     return sum(p ** log(1/p, 2) for p in probabilidadSimbolos)
 
-def longitudMedia():
-    pass
+def longitudMedia(probabilidadSimbolos,codificacion):
+    return sum(p * len(c) for p,c in zip(probabilidadSimbolos,codificacion))
 
-def isCompacto():
-    pass
+def isCompacto(probabilidadSimbolos,codificacion):
+    return longitudMedia(probabilidadSimbolos,codificacion) >= entropia(probabilidadSimbolos)
 
-def cumpleKraft():
-    pass
+def cumpleKraft(codificacion):
+    return sum(2 ** -len(c) for c in codificacion)
