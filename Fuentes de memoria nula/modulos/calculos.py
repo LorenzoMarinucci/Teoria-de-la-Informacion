1d0b014dc8e7c075762fa5d7f216c759d3a10daf
Lucia <62451140+LuPalomeque@users.noreply.github.com>
2020-10-22 20:58:53 -0300

    Arreglos Calculos Memoria Nula

--- Fuentes de memoria nula/modulos/calculos.py
+++ Fuentes de memoria nula/modulos/calculos.py
@@ -5,24 +5,24 @@ Created on 22 sep. 2020
 '''
 import math
 
-def cantInformacion(diccionario):
-    '''Dado el diccionario de simbolos, se procede a calcular la cantidad de informacion aportada por 
+def cantInformacion(fuente):
+    '''Dada la fuente de simbolos, se procede a calcular la cantidad de informacion aportada por
     cada simbolo.
      
     Parametros:
-        Diccionario:Objeto que contiene a los simbolos como claves y la distribucion de probabilidad 
+        Fuente:Objeto que contiene a los simbolos como claves y la distribucion de probabilidad
         como valor.
-    Retorno:Diccionario con la cantidad de informacion aportada por simbolo.
+    Retorno:Fuente con la cantidad de informacion aportada por simbolo.
     '''
     cantInformacion = {}
-    for simbolo in diccionario:
-        if (diccionario[simbolo]):
-            cantInformacion[simbolo] = math.log(1/diccionario[simbolo], 2)
+    for simbolo in fuente:
+        if (fuente[simbolo]):
+            cantInformacion[simbolo] = math.log(1/fuente[simbolo], 2)
         else:
             cantInformacion[simbolo] = 0
     return cantInformacion
 
-def entropia(cantInformacion, simbolos):
+def entropia(cantInformacion,probabilidades):
     ''' Dada la cantidad de informacion aportada por cada elemento, almacenada en un diccionario, se procede a
         calcular la entropia realizando la sumatoria de cada producto entre la cantidad de informacion y
         probabilidad de ocurrencia de un simbolo determinado.
@@ -35,6 +35,6 @@ def entropia(cantInformacion, simbolos):
     Retorno:Valor de la entropia.
     '''
     entropia = 0
-    for simbolo in simbolos:
-        entropia += simbolos[simbolo] * cantInformacion[simbolo]
+    for simbolo in probabilidades:
+        entropia += probabilidades[simbolo] * cantInformacion[simbolo]
     return entropia
