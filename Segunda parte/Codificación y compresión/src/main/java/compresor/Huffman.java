cf8f988ac50c0397e19b76107d2e4df250548e44
guidog378 <54630740+guidog378@users.noreply.github.com>
2020-11-30 18:35:43 -0300

    Add files via upload

--- Segunda parte/Codificación y compresión/src/main/java/compresor/Huffman.java
+++ Segunda parte/Codificación y compresión/src/main/java/compresor/Huffman.java
@@ -1,54 +1,47 @@
 package compresor;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Huffman extends Compresor{
 
     public static HashMap<Character,String> generarAlfabetoCodigo(HashMap<Character,Float> probabilidades){
 		if(probabilidades.size() == 2) {
-	    	Float minimaProb = (float) 2;
-	    	Float maximaProb = (float) -1;
-	    	Character minimoChar = null;
-	    	Character maximoChar = null;
-	    	HashMap<Character,String> codificacionH = new HashMap<Character,String>();
-	    	Set<Character> claves = probabilidades.keySet();
-	    	for(Character clave:claves) {
-	    		Float probabilidad = probabilidades.get(clave);
-	    		if(probabilidad < minimaProb) {
-	    			minimaProb = probabilidad;
-	    			minimoChar = clave;
-	    		}
-	    		if(probabilidad > maximaProb) {
-	    			maximaProb = probabilidad;
-	    			maximoChar = clave;
-	    		}
-	    	}
-	    	codificacionH.put(minimoChar, "0");
-	    	codificacionH.put(maximoChar, "1");
-	    	return codificacionH;
-	    }
-	    else if(probabilidades.size() == 1) {
-	    	HashMap<Character,String> codificacionH = new HashMap<Character,String>();
-			Set<Character> claves = probabilidades.keySet();
-			for(Character clave:claves) 
-				codificacionH.put(clave, "0");
+			HashMap<Character,String> codificacionH = new HashMap<Character,String>();
+			Iterator<Character> itSimbolos = probabilidades.keySet().iterator();
+			Character simbolo1 = itSimbolos.next(), simbolo2 = itSimbolos.next();
+			if(probabilidades.get(simbolo1) < probabilidades.get(simbolo2)) {
+				codificacionH.put(simbolo1, "0");
+	    		codificacionH.put(simbolo2, "1");
+			} else {
+				codificacionH.put(simbolo1, "1");
+		    	codificacionH.put(simbolo2, "0");
+			}
 			return codificacionH;
-	    }
-	    else {
-	        HashMap<Character,Float> copiaProbabilidades = (HashMap<Character,Float>)probabilidades.clone();
-	        Character minimo1 = getClaveMinimo(copiaProbabilidades);
-	        Float suma = copiaProbabilidades.get(minimo1);
-	        copiaProbabilidades.remove(minimo1);
-	        Character minimo2 = getClaveMinimo(copiaProbabilidades);
-	        suma += copiaProbabilidades.get(minimo2);
-	        copiaProbabilidades.remove(minimo2);
-	        copiaProbabilidades.put(minimo1, suma);    
-	        HashMap<Character,String> codificacionH = generarAlfabetoCodigo(copiaProbabilidades);
-	        String nuevoCodigoLista = codificacionH.get(minimo1);
-	        codificacionH.remove(minimo1);
-	        codificacionH.put(minimo1, nuevoCodigoLista+"0");
-	        codificacionH.put(minimo2, nuevoCodigoLista+"1");
+	    } else {
+	    	HashMap<Character,Float> probabilidadesReducida = (HashMap<Character,Float>)probabilidades.clone();
+	        //Se busca el simbolo con la menor probabilidad.
+	        Character minimoSimbolo1 = getClaveMinimo(probabilidadesReducida);
+	        Float sumaProbabilidades = probabilidadesReducida.get(minimoSimbolo1);
+	        probabilidadesReducida.remove(minimoSimbolo1);
+	        //Se busca el siguiente simbolo con menor probabilidad.
+	        Character minimoSimbolo2 = getClaveMinimo(probabilidadesReducida);
+	        sumaProbabilidades += probabilidadesReducida.get(minimoSimbolo2);
+	        probabilidadesReducida.remove(minimoSimbolo2);
+	        //Se conforma un simbolo cuya probabilidad es la suma de las probabilidades de los anteriores.
+	        //Y queda conformada la fuente reducida.
+	        //ACLARACION:Se agrega un simbolo que ya antes existia,ya que se asegura que el mismo no existe dentro de la fuente reducida.
+	        //Tambien se debe a que el tipo de datos que usamos como clave en el hashMap es un caracter,si fuera string,podriamos generar
+	        //un nuevo simbolo como combinacion de los anteriores.
+	        probabilidadesReducida.put(minimoSimbolo1, sumaProbabilidades);   
+	        //Se llama recursivamente al metodo con la fuente reducida.
+	        HashMap<Character,String> codificacionH = generarAlfabetoCodigo(probabilidadesReducida);
+	        String nuevoCodigoLista = codificacionH.get(minimoSimbolo1);
+	        //Cuando se retorna luego de tener la codificacion base,se comienzan a agregar a la derecha ceros y unos a los codigos de los 
+	        //simbolos que dieron lugar al simbolo "nuevo".
+	        codificacionH.replace(minimoSimbolo1, nuevoCodigoLista+"0");
+	        codificacionH.put(minimoSimbolo2, nuevoCodigoLista+"1");
 	        return codificacionH;
 	    }
 	}
--- Segunda parte/Codificación y compresión/src/main/java/compresor/Shannon_Fano.java
+++ Segunda parte/Codificación y compresión/src/main/java/compresor/Shannon_Fano.java
@@ -2,6 +2,7 @@ package compresor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Shannon_Fano extends Compresor{
@@ -13,38 +14,26 @@ public class Shannon_Fano extends Compresor{
     	//Si la cantidad de elementos del hashMap probabilidades es igual a 2 se busca el de mayor probabilidad 
     	//para asignarle un 1 y el de minima probabilidad para asignarle un 0.Luego estas asignaciones se almacenan 
     	//en un hashMap utilizando como clave el simbolo y como contenido la asignacion antes mencionada en forma de string.
-    	//Por supuesto las asignaciones antes mencionadas son absolutamente arbitrarias.
-    	//Finalmente se retorna el hashMap antes descripto.
+    	//Por supuesto las asignaciones de 1 y 0 son absolutamente arbitrarias.
         if(probabilidades.size()==2) {
-        	Float minimaProb = (float) 2;
-	    	Float maximaProb = (float) -1;
-	    	Character minimoChar = null;
-	    	Character maximoChar = null;
-	    	HashMap<Character,String> codificacionSF = new HashMap<Character,String>();
-	    	Set<Character> claves = probabilidades.keySet();
-	    	for(Character clave:claves) {
-	    		Float probabilidad = probabilidades.get(clave);
-	    		if(probabilidad < minimaProb) {
-	    			minimaProb = probabilidad;
-	    			minimoChar = clave;
-	    		}
-	    		if(probabilidad >= maximaProb) {
-	    			maximaProb = probabilidad;
-	    			maximoChar = clave;
-	    		}
-	    	}
-	    	codificacionSF.put(minimoChar, "0");
-	    	codificacionSF.put(maximoChar, "1");
-	    	return codificacionSF;
+        	HashMap<Character,String> codificacionSH = new HashMap<Character,String>();
+			Iterator<Character> itSimbolos = probabilidades.keySet().iterator();
+			Character simbolo1 = itSimbolos.next(), simbolo2 = itSimbolos.next();
+			if(probabilidades.get(simbolo1) < probabilidades.get(simbolo2)) {
+				codificacionSH.put(simbolo1, "0");
+	    		codificacionSH.put(simbolo2, "1");
+			} else {
+				codificacionSH.put(simbolo1, "1");
+		    	codificacionSH.put(simbolo2, "0");
+			}
+			return codificacionSH;
         } 
         //Si la cantidad de elementos del hashMap probabilidades es igual a 1,directamente se le asigna al unico elemento un 1.
-        //Esto se almacena en un hashMap que usa de claves a los simbolos y como valores a la asignacion en forma de string.
-        //Finalmente se retorna el hashMap antes descripto.
         else if(probabilidades.size()==1) {
         	HashMap<Character,String> codificacionSF = new HashMap<Character,String>();
-			Set<Character> claves = probabilidades.keySet();
-			for(Character clave:claves) 
-				codificacionSF.put(clave, "1");
+			Set<Character> simbolos = probabilidades.keySet();
+			for(Character simbolo:simbolos) 
+				codificacionSF.put(simbolo, "1");
 			return codificacionSF;
         } 
         //Si la cantidad de elementos es mayor a 2 entonces lo que se procede a hacer es:
@@ -58,23 +47,23 @@ public class Shannon_Fano extends Compresor{
         //	de esa codificacion un 0 delante.
         //7:Por ultimo se arma un conjunto unico y se retorna.
         else {
-        	ArrayList<Character> claveOrdenada = ordenarProbabilidades(probabilidades);
-        	HashMap<Character,Float> conjunto1 = generaConjunto1(probabilidades,claveOrdenada);
-        	HashMap<Character,String> mapCodigo1 = generarAlfabetoCodigo(conjunto1);
-        	HashMap<Character,String> mapCodigo2 = generarAlfabetoCodigo(generaConjunto2(conjunto1,probabilidades));
-        	Set<Character> claves;
-        	if(mapCodigo1.size() > 1) {
-        		claves = mapCodigo1.keySet();
-        		for(Character clave:claves)
-        			mapCodigo1.replace(clave, "1"+mapCodigo1.get(clave));
+        	ArrayList<Character> simbolosOrdenados = ordenarProbabilidades(probabilidades);
+        	HashMap<Character,Float> conjunto1 = generaConjunto1(probabilidades,simbolosOrdenados);
+        	HashMap<Character,String> codificacion1 = generarAlfabetoCodigo(conjunto1);
+        	HashMap<Character,String> codificacion2 = generarAlfabetoCodigo(generaConjunto2(conjunto1,probabilidades));
+        	Set<Character> simbolos;
+        	if(codificacion1.size() > 1) {
+        		simbolos = codificacion1.keySet();
+        		for(Character simbolo:simbolos)
+        			codificacion1.replace(simbolo, "1"+codificacion1.get(simbolo));
         	}
-        	if(mapCodigo2.size() > 1) {
-        		claves = mapCodigo2.keySet();
-        		for(Character clave:claves)
-        			mapCodigo2.replace(clave, "0"+mapCodigo2.get(clave));
+        	if(codificacion2.size() > 1) {
+        		simbolos = codificacion2.keySet();
+        		for(Character simbolo:simbolos)
+        			codificacion2.replace(simbolo, "0"+codificacion2.get(simbolo));
         	}
-        	mapCodigo1.putAll(mapCodigo2);
-        	return mapCodigo1;
+        	codificacion1.putAll(codificacion2);
+        	return codificacion1;
         }
     }
 
@@ -84,10 +73,10 @@ public class Shannon_Fano extends Compresor{
     //conjunto 1.
 	private static HashMap<Character, Float> generaConjunto2(HashMap<Character, Float> conjunto1, HashMap<Character,Float> probabilidades) {
 		HashMap<Character,Float> conjunto2 = new HashMap<Character,Float>();
-		Set<Character> claves = probabilidades.keySet();
-    	for(Character clave:claves) {
-    		if(!conjunto1.containsKey(clave))
-    			conjunto2.put(clave, probabilidades.get(clave));
+		Set<Character> simbolos = probabilidades.keySet();
+    	for(Character simbolo:simbolos) {
+    		if(!conjunto1.containsKey(simbolo))
+    			conjunto2.put(simbolo, probabilidades.get(simbolo));
     	}
 		return conjunto2;
 	}
@@ -96,53 +85,51 @@ public class Shannon_Fano extends Compresor{
 	//Los pasos que sigue para lograrlo son:
 	//1:Calcular el total de la suma de las probabilidades del conjunto de probabilidades.
 	//2:Se divide el total en 2 para tener una medida de cuando se deberia parar de agregar elementos al conjunto 1.
-	//3:Se recorre todo el conjunto de probabilidades de manera ordenada y para cada simbolo se calculan dos diferencias,las cuales 
-	//  determinan si se tiene una mejor diferencia respecto del valor medio al agregar un simbolo con su probabilidad al conjunto 
-	//	1 o al conjunto 2.
-	private static HashMap<Character, Float> generaConjunto1(HashMap<Character, Float> probabilidades,ArrayList<Character> claveOrdenada) {
+	//3:Se recorre todo el conjunto de probabilidades de manera ordenada y para cada simbolo se calculan las probabilidades acumuladas,
+	//  las cuales determinan si se tiene una mejor diferencia al agregar un simbolo con su probabilidad al conjunto 1 o al conjunto 2.
+	private static HashMap<Character, Float> generaConjunto1(HashMap<Character, Float> probabilidades,ArrayList<Character> simbolosOrdenados) {
 		HashMap<Character,Float> conjunto1 = new HashMap<Character,Float>();
+		Iterator<Character> itSimbolos = simbolosOrdenados.iterator();
+		Character simbolo;
 		Float suma = (float) 0;
-		Float mitadTotal = (float) 0;
-        Float total = (float) 0,diferencia1,diferencia2;
-        Float probabilidad;
-        boolean mejorDiferencia;
-        for(Character clave:claveOrdenada) 
-        	total += probabilidades.get(clave);
-        mitadTotal = total/2;
-        for(Character clave:claveOrdenada) {
-        	probabilidad = probabilidades.get(clave);
-        	diferencia1 = Math.abs(suma + probabilidad - mitadTotal);
-            diferencia2 = Math.abs((total - suma) - mitadTotal);
-            if(diferencia1 < diferencia2)
-                mejorDiferencia = true;
-            else
-                mejorDiferencia = false;
-            suma += probabilidad;          
-            if(suma <= mitadTotal || mejorDiferencia || conjunto1.isEmpty())
-                conjunto1.put(clave, probabilidad);
-        }
+	    Float total = (float) 0,probAcumulada1,probAcumulada2;
+	    Float probabilidad;
+	    boolean mejorDiferencia = true;
+	    for(Character simboloOrd:simbolosOrdenados) 
+	       	total += probabilidades.get(simboloOrd);
+	    while(itSimbolos.hasNext() && mejorDiferencia) {
+	       	simbolo = itSimbolos.next();
+	       	probabilidad = probabilidades.get(simbolo);
+	       	probAcumulada1 = Math.abs(suma + probabilidad);
+	        probAcumulada2 = Math.abs(total - suma);
+	        if(probAcumulada1 >= probAcumulada2)
+	            mejorDiferencia = false;
+	        suma += probabilidad;          
+	        if(mejorDiferencia || conjunto1.isEmpty())
+	        	conjunto1.put(simbolo, probabilidad);
+	    }
 		return conjunto1;
 	}
-
+	
   //El metodo ordena las probabilidades de mayor a menor y devuelve una lista de caracteres que reprensenta dicho orden. 
 	public static ArrayList<Character> ordenarProbabilidades(HashMap<Character, Float> probabilidades) {
 		int tamanio = probabilidades.size();
 		Float maximo = (float) -1,probabilidad;
-		Character maxClave = null;
+		Character maxSimbolo = null;
 		ArrayList<Character> probOrdenada = new ArrayList<Character>();
 		HashMap<Character, Float> copiaProbabilidades = (HashMap<Character, Float>) probabilidades.clone();
-		Set<Character> claves;
+		Set<Character> simbolos;
 		while(tamanio != probOrdenada.size()) {
-			claves = copiaProbabilidades.keySet();
-			for(Character clave:claves) {
-				probabilidad = copiaProbabilidades.get(clave);
+			simbolos = copiaProbabilidades.keySet();
+			for(Character simbolo:simbolos) {
+				probabilidad = copiaProbabilidades.get(simbolo);
 				if(probabilidad > maximo) {
 					maximo = probabilidad;
-					maxClave = clave;
+					maxSimbolo = simbolo;
 				}
 			}
-			copiaProbabilidades.remove(maxClave);
-			probOrdenada.add(maxClave);
+			copiaProbabilidades.remove(maxSimbolo);
+			probOrdenada.add(maxSimbolo);
 			maximo = (float) -1;
 		}
 		return probOrdenada;
